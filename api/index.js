79bed4ffa9edb3896be209c51eec938b1fe3d5f4
Your Name <jadabonajem5@gmail.com>
2026-06-27 14:36:12 +0300

    changed redirect to collaborator

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
-  const target = 'https://urp1wju59bprod9bhflio9qjqaw1ks8h.oastify.com';
+  const target = 'https://localhost';
 
   res.status(302);
   res.setHeader('Location', target);
