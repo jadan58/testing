886f02174ea0a2b06f105b613c2f898ebd552b62
Your Name <jadabonajem5@gmail.com>
2026-06-27 15:35:24 +0300

    changed redirect

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
-  const target = 'https://localhost';
+  const target = 'https://7f000001.c0a80001.rbndr.us';
 
   res.status(302);
   res.setHeader('Location', target);
