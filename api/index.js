b00499a2af062d92d0c28204df3d4f2d08fd0a6f
Your Name <jadabonajem5@gmail.com>
2026-06-28 14:52:12 +0300

    removed redirect

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
-module.exports = (req, res) => {
-  const target = 'https://7f000001.c0a80001.rbndr.us';
+// module.exports = (req, res) => {
+//   const target = 'https://7f000001.c0a80001.rbndr.us';
 
-  res.status(302);
-  res.setHeader('Location', target);
-  res.end();
-};
+//   res.status(302);
+//   res.setHeader('Location', target);
+//   res.end();
+// };
