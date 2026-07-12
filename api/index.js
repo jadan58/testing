c7102642beae0467bb73ccae8f110dbc8edf5163
Your Name <jadabonajem5@gmail.com>
2026-07-12 22:21:30 +0300

    test

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
-  const target = 'https://3gkuzir5ffr85rpcck4gzya6jxpodh16.oastify.com';
+  const target = 'https://localhost';
 
   res.status(302);
   res.setHeader('Location', target);
